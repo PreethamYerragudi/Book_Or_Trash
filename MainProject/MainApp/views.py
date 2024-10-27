a273557106c758f0cff36e372e5642e629ae1e31
PreethamYerragudi <preetham.yerragudi@gmail.com>
2024-10-26 22:18:16 -0400

    Throwing Works

--- MainProject/MainApp/views.py
+++ MainProject/MainApp/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def mainPage(request):
-    return render(request, 'index.html')
+    content = {
+        "image": "https://images.gr-assets.com/books/1361975680m/2657.jpg"
+    }
+    return render(request, 'index.html', content)
