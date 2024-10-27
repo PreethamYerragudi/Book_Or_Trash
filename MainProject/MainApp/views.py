affc011a136c538b96192139d580b9e3b98be143
PreethamYerragudi <preetham.yerragudi@gmail.com>
2024-10-26 23:15:33 -0400

    sent info via requests

--- MainProject/MainApp/views.py
+++ MainProject/MainApp/views.py
@@ -6,3 +6,10 @@ def mainPage(request):
         "image": "https://images.gr-assets.com/books/1361975680m/2657.jpg"
     }
     return render(request, 'index.html', content)
+
+def update_counter(request):
+    global is_left_side
+    if request.method == 'POST':
+        is_left_side = request.POST.get('isLeftSide') == 'true'
+    print(is_left_side)
+    return render(request, "book.html")
