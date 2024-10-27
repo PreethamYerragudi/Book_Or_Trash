33e3dd1f360a839790e8f176337702f136abd886
Jianing Wang <24wangj@gmail.com>
2024-10-27 09:07:25 -0400

    Summary generator and recommender

--- Summary.py
+++ Summary.py
@@ -1,31 +1,46 @@
 import openai
-import PyPDF2
 import os
 
+from openai import OpenAI
 
-openai.api_key = os.environ['InternKey']
+loved = []
+hated = []
 
+# openai.api_key = os.environ['InternKey']
 
-def makeSummary(input):
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key = "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"
+)
+
+def makeSummary(title):
     # Make a request to the ChatGPT API
-    return openai.ChatCompletion.create(
+    return client.chat.completions.create(
         model="gpt-3.5-turbo",  # You can also use "gpt-4" if you have access
         messages=[
-            {"role": "user", "content": "Generate a summary of the following book that is 50 words or less "+ input}
-     ]
+            {"role": "user",
+             "content": "Generate a summary of the following book that is 100 words or less " + title}
+     ],
     
-    )
+    ).choices[0].message.content
+
 
 def finalReccomendation(booked, trashed):
-    return openai.ChatCompletion.create(
+    return client.chat.completions.create(
         model="gpt-3.5-turbo",  # You can also use "gpt-4" if you have access
         messages=[
-            {"role": "user", "content": "Generate a book recomendation based on this list of liked books: "+"".join(booked)+" and this list of disliked books: "+ "".join(trashed)+" make the output just the title of the reccomendation"}
-     ]
+            {"role": "user", "content": "Generate a book recomendation based on this list of liked books: "+"".join(booked)+" and this list of disliked books: "+ "".join(trashed)}
+     ],
     
-    )
+    ).choices[0].message.content
+
+
+def add_book(title, liked):
+    if liked:
+        loved.append(title + ", ")
+    else:
+        hated.append(title + ", ")
 
 loved = ["The Hobbit,", "The Magician's Nephew,", "Peter And Wendy,", "RedWall,", "Eragorn,", "A Game Of Thrones,", "Jade City,", "The StarDust Theif,", "The Buried Giant,", "Howl's Moving Castle,"]
 hated = ["The Jungle,","Silent Spring,","Follies,","Hidden Figures,"," Into The Wind,","Black Like Me,","In Cold Blood,","A Walk In The Woods,","In The Time Of Butterflies,","Into Thin Air,"]
-print(finalReccomendation(loved,hated)['choices'][0]['message']['content'])
 
